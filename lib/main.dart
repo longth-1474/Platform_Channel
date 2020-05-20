56e5bf8edb7ea3c92f59eb121aee1c448177892b
hoanglong96 <long11hq@gmail.com>
2020-05-20 22:31:56 +0700

    Refs demo platform channel

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_platform_channel/channel/event_channel.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DemoEventChannel(),
+    );
+  }
+}
